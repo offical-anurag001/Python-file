4cd382d012b67a18ba1f7481994c63a91ae465fb
offical-anurag001 <anuragkumarravi02@gmail.com>
2025-11-21 23:35:56 +0530

    Add files via upload

---
+++ Task  1.py
@@ -0,0 +1,23 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #  Mathematical operation
+>>> 
+>>> number1=100
+>>> number2=10
+>>> 
+>>> # + -> Addition
+>>> number1+number2
+110
+>>> 
+>>> # - -> Subtraction
+>>> number1-number2
+90
+>>> 
+>>> 
+>>> # *->multiplication
+>>> number1*number2
+1000
+>>> 
+>>> # /-> Division
+>>> number1/number2
+10.0
