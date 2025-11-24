22de9542c24dd28a38afae4b58b9848556526e91
offical-anurag001 <anuragkumarravi02@gmail.com>
2025-11-24 11:21:35 +0530

    Add files via upload

---
+++ TASK--1.py
@@ -0,0 +1,26 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+... Enter "help" below or click "Help" above for more information.
+... >>> # Program to perform basic mathematical operations
+... ... 
+... ... # Using fixed numbers
+... ... num1 = 100
+... ... num2 = 20
+... ... 
+... ... # Performing operations
+... ... addition = num1 + num2
+... ... subtraction = num1 - num2
+... ... multiplication = num1 * num2
+... ... division = num1 / num2
+... ... 
+... ... # Displaying results
+... ... print("--- Results ---")
+... ... print("Addition:", addition)
+... ... print("Subtraction:", subtraction)
+... ... print("Multiplication:", multiplication)
+... ... print("Division:", division)
+... >>> --- Results ---
+... ... Addition: 120
+... ... Subtraction: 80
+... ... Multiplication: 2000
